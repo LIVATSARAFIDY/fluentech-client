90a10d8af675438e1910d27dd92df446f3ec3501
LIVATSARAFIDY <pierrethomas2liva@gmail.co>
2024-08-29 16:16:31 +0300

    fetch additional characters from backend ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
-import {Container,Box,Typography,Avatar,Pagination,Stack,Skeleton} from '@mui/material'
+import {Container,Box,Typography,Avatar,Pagination,Stack,Skeleton,Button} from '@mui/material'
 import { useEffect, useState } from 'react';
 import CharacterMarvelModel from './models/Character';
-import CHARACTERS from './mock-data';
+import CHARACTERS_100_FIRST from './mock-data';
 
 function App() {
   const [ loadingCharacters,setLodingCharacters ] = useState<boolean>(true)
   const [ charactersToDisplay, setCharactersToDisplay ] = useState<CharacterMarvelModel[]|null>(null)
   const [ nbTotalOfCharacters, setnbTotalOfCharacters ] = useState<number>(0)
-  const [pageActive,setPageActive] = useState<number>(1)
+  const [ pageActive,setPageActive] = useState<number>(1)
+  const [ perso,setPerso ] = useState<CharacterMarvelModel[]>(CHARACTERS_100_FIRST)
+
+  const loadMoreCharacters = async () => {
+    setLodingCharacters(true)
+    
+    const persoTemp = [...perso]
+
+    try {
+      const request = await fetch(
+        `http://localhost:5000/marvel/characters?nbCharacter=${perso.length}`,
+        {
+          method: 'GET',
+          headers:{
+            "Content-Type":"application/json"
+          }
+        }
+      )
+      const response = await request.json()
+
+      persoTemp.push(...response.data as CharacterMarvelModel[])
+      setnbTotalOfCharacters(persoTemp.length)
+      setPerso(persoTemp)
+      setLodingCharacters(false)
+    } catch (error) {
+      alert('error loading')
+      setLodingCharacters(true)
+    }
+  }
 
   const handleChangePage = (page:number) => {
-    const allCharacters = [...CHARACTERS]
+    const allCharacters = [...perso]
     const beginningIndex = ((page-1)*5)
     const charactersToDisplayTemp = allCharacters.splice(beginningIndex,5)
     setCharactersToDisplay(charactersToDisplayTemp)
@@ -18,13 +46,12 @@ function App() {
   }
 
   useEffect(() =>{
-    setTimeout(() => {
-      const allCharacters = [...CHARACTERS]
+      const allCharacters = [...perso]
       const charactersToDisplayTemp = allCharacters.splice(0,5)
-      setnbTotalOfCharacters(CHARACTERS.length)
+      setnbTotalOfCharacters(perso.length)
       setCharactersToDisplay(charactersToDisplayTemp)
       setLodingCharacters(false)
-    }, 1000);
+    
   },[])
 
   return (
@@ -33,7 +60,7 @@ function App() {
         <Typography sx={{fontFamily:"fantasy !important",fontSize:{xs:"1em",md:"4em"}}} > The characters of the marvel world </Typography>
       </Box>
       <Box className="container-characters" sx={{
-          display:"flex", flexDirection:{xs:"column",md:"row"}, border:"2px solid white",
+          display:"flex", flexDirection:{xs:"column",md:"row"},
           alignItems:"center",
           justifyContent:"center"
         }} 
@@ -54,12 +81,15 @@ function App() {
                 (charactersToDisplay !== null && charactersToDisplay.length > 0) && (
                   <>
                     {
-                      charactersToDisplay.map( charcter => (
-                        <Avatar
-                          alt="charcter-marvel"
-                          src={charcter.imageUrl}
-                          sx={{ width: 200, height: 200, border:"1px solid #732380", m:1 }}
-                        />
+                      charactersToDisplay.map( (charcter,index) => (
+                        <Box key={index} >
+                          <Avatar
+                            alt="charcter-marvel"
+                            src={charcter.imageUrl}
+                            sx={{ width: 200, height: 200, border:"1px solid #732380", m:1 }}
+                          />
+                          <Typography sx={{textAlign:"center"}} > {charcter.name} </Typography>
+                        </Box>
                       ) ) 
                     }
                   </>
@@ -72,8 +102,7 @@ function App() {
       <Box className="pagination" sx={{display:"flex",justifyContent:"center",mt:2}} >
         <Stack spacing={2}>
           <Pagination
-            // count={1564/5} 
-            count={nbTotalOfCharacters/5} 
+            count={Math.ceil(nbTotalOfCharacters/5)} 
             variant="outlined" 
             color="secondary" 
             boundaryCount={9}
@@ -85,6 +114,25 @@ function App() {
             onChange={(e,page) => handleChangePage(page)}
           />
         </Stack>
+      </Box>
+      <Box sx={{display:"flex",justifyContent:"center",mt:2}}>
+        <Button variant="outlined" sx={
+          {
+            color: '#732380',              
+            borderColor: '#732380',        
+            backgroundColor: '#2c1d2d',    
+            '&:hover': {
+              backgroundColor: '#2c1d2d',  
+              borderColor: '#732380',      
+              color: '#ffffff',            
+            }
+           }
+          }  
+          disabled={ perso.length >= 1564 && true }
+          onClick={() => loadMoreCharacters()}
+        >
+          Load more characters
+        </Button>
       </Box>
     </Container>
   );
--- src/mock-data.tsx
+++ src/mock-data.tsx
@@ -1,12 +1,13 @@
 import CharacterMarvelModel from "./models/Character"
 
-const CHARACTERS: CharacterMarvelModel[] = [
-    {
+const CHARACTERS_100_FIRST: CharacterMarvelModel[] = [
+    //0
+	{
 		name: "3-D Man",
 		description: "",
 		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
 	},
-    {
+	{
 		name: "A-Bomb (HAS)",
 		description: "Rick Jones has been Hulk's best bud since day one, but now he's more than a friend...he's a teammate! Transformed by a Gamma energy explosion, A-Bomb's thick, armored skin is just as strong and powerful as it is blue. And when he curls into action, he uses it like a giant bowling ball of destruction! ",
 		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16.jpg"
@@ -100,9 +101,420 @@ const CHARACTERS: CharacterMarvelModel[] = [
 		name: "Air-Walker (Gabriel Lan)",
 		description: "",
 		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	//19
+	//20
+	{
+		name: "Ajak",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/2/80/4c002f35c5215.jpg"
+	},
+	{
+		name: "Ajaxis",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/70/4c0035adc7d3a.jpg"
+	},
+	{
+		name: "Akemi",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Alain",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Albert Cleary",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Albion",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Alex Power",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/9/50/4ce5a385a2e82.jpg"
+	},
+	{
+		name: "Alex Wilder",
+		description: "Despite being the only one of the Runaways without any superhuman abilities or tech, Alex Wilder became the de facto leader of the group due to his natural leadership skills and intellect, as well as prodigy-level logic and strategy.",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/2/c0/4c00377144d5a.jpg"
+	},
+	{
+		name: "Alexa Mendez",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Alexander Pierce",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Alice",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/6/70/4cd061e6d6573.jpg"
+	},
+	{
+		name: "Alicia Masters",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/4c003d40ac7ae.jpg"
+	},
+	{
+		name: "Alpha Flight",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/1/60/52695277ee088.jpg"
+	},
+	{
+		name: "Alpha Flight (Ultimate)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Alvin Maker",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Amadeus Cho",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/3/80/520288b9cb581.jpg"
+	},
+	{
+		name: "Amanda Sefton",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Amazoness",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "American Eagle (Jason Strongbow)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/f/80/4ce5a6d8b8f2a.jpg"
+	},
+	{
+		name: "Amiko",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	//39
+	//40
+	{
+		name: "Amora",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Amphibian (Earth-712)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Amun",
+		description: "Amun is a ruthless teenage assassin, employed by the Sisterhood of the Wasp to serve under the mage Vincent after Araña interrupted the ritual to initiate the Wasp's new chosen one.",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Ancient One",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/b0/4ce59ea2103ac.jpg"
+	},
+	{
+		name: "Ancient One (Ultimate)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Angel (Angel Salvadore)",
+		description: "Driven out of home by her abusive step-father, fourteen-year old Angel Salvadore slept in the woods, where her mutant nature manifested itself in the form of a protective cocoon.",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Angel (Thomas Halloway)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/d/03/531769834b15f.jpg"
+	},
+	{
+		name: "Angel (Ultimate)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/4/50/531769ae4399f.jpg"
+	},
+	{
+		name: "Angel (Warren Worthington III)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Angela (Aldrif Odinsdottir)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/7/00/545a82f59dd73.jpg"
+	},
+	{
+		name: "Anita Blake",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/2/a0/4c0038fa14452.jpg"
+	},
+	{
+		name: "Anne Marie Hoag",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Annihilus",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/5/f0/528d31f20a2f6.jpg"
+	},
+	{
+		name: "Anole",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/9/20/4c002e635ddd9.jpg"
+	},
+	{
+		name: "Ant-Man (Eric O'Grady)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/9/c0/53176aa9df48d.jpg"
+	},
+	{
+		name: "Ant-Man (Hank Pym)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Ant-Man (Scott Lang)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/e/20/52696868356a0.jpg"
+	},
+	{
+		name: "Anthem",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Apocalypse",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/f/e0/526166076a1d0.jpg"
+	},
+	{
+		name: "Apocalypse (Ultimate)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	//59
+	//60
+	{
+		name: "Aqueduct",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/5/50/4c0035b3630cd.jpg"
+	},
+	{
+		name: "Arachne",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/70/5269526591794.jpg"
+	},
+	{
+		name: "Araña",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Arcade",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/2/a0/4c0042091ab69.jpg"
+	},
+	{
+		name: "Arcana",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Archangel",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/8/03/526165ed93180.jpg"
+	},
+	{
+		name: "Arclight",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/5/f0/4c0042067fd8b.jpg"
+	},
+	{
+		name: "Ares",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/c/10/535ff3daea603.jpg"
+	},
+	{
+		name: "Argent",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Armadillo",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/2/40/4c0032754da02.jpg"
+	},
+	{
+		name: "Armor (Hisako Ichiki)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/9/20/4c002e6cbf990.jpg"
+	},
+	{
+		name: "Armory",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Arnim Zola",
+		description: "The frail, dwarfish Arnim Zola was born in 1930s Switzerland where he became the world's leading biochemist and genetic engineer.",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/8/b0/4c00393a4cb7c.jpg"
+	},
+	{
+		name: "Arsenic",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/8/c0/4c00359a2be7b.jpg"
+	},
+	{
+		name: "Artiee",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Asgardian",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Askew-Tronics",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Asylum",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Atlas (Team)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Attuma",
+		description: "Attuma was born into the tribe of Homo mermanus who eschewed civilization to live as nomadic barbarians. For some unrecorded reason, Attuma was endowed with strength far surpassing that of his people.",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/9/90/4ce5a862a6c48.jpg"
+	},
+	//79
+	//80
+	{
+		name: "Aurora",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/f/10/4c004203f1072.jpg"
+	},
+	{
+		name: "Avalanche",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/f/10/4c0042010d383.jpg"
+	},
+	{
+		name: "Avengers",
+		description: "Earth's Mightiest Heroes joined forces to take on threats that were too big for any one hero to tackle. With a roster that has included Captain America, Iron Man, Ant-Man, Hulk, Thor, Wasp and dozens more over the years, the Avengers have come to be regarded as Earth's No. 1 team.",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/9/20/5102c774ebae7.jpg"
+	},
+	{
+		name: "Avengers (Ultimate)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Azazel (Mutant)",
+		description: "A mutant from biblical times, Azazel is the ruler of the Neyaphem and claims that the Earth and everything on it belongs to him.",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Balder",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/f0/4ce5a7c2814ba.gif"
+	},
+	{
+		name: "Banshee",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/03/52740e4619f54.jpg"
+	},
+	{
+		name: "Banshee (Theresa Rourke)",
+		description: "The daughter of former X-Men member Sean Cassidy, a.k.a. Banshee, and Maeve Rourke, Theresa Rourke was raised by her first cousin once removed, mutant terrorist Thomas Cassidy, a.k.a. Black Tom.",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/c0/4ce5a1a50e56b.jpg"
+	},
+	{
+		name: "Baron Mordo (Karl Mordo)",
+		description: "Born in 1921, Karl Mordo was the son of Nikolai Mordo.  He vowed that, as the second Baron Mordo, he would achieve his father’s hopes and more.",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/f/90/4ce5a86dba8a8.jpg"
+	},
+	{
+		name: "Baron Strucker",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/8/80/4c0041fb5a90d.jpg"
+	},
+	{
+		name: "Baron Zemo (Heinrich Zemo)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/9/60/4c0041f84c9fe.jpg"
+	},
+	{
+		name: "Baron Zemo (Helmut Zemo)",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/5/a0/4c0035890fb0a.jpg"
+	},
+	{
+		name: "Baroness S'Bak",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Barracuda",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Bart Rozum",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Bastion",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/d/80/52695253215f4.jpg"
+	},
+	{
+		name: "Batroc the Leaper",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/c/80/4ce59eb840da5.gif"
+	},
+	{
+		name: "Battering Ram",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708.gif"
+	},
+	{
+		name: "Battlestar",
+		description: "Lamar Hoskins was once a wrestler who was given superhuman strength by the Power Broker.",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	},
+	{
+		name: "Beak",
+		description: "",
+		imageUrl: "http://i.annihil.us/u/prod/marvel/i/mg/5/90/4c0040b8329ad.jpg"
 	}
+	
+	//99
 ]
 
-export default CHARACTERS
+export default CHARACTERS_100_FIRST
+
 
  
